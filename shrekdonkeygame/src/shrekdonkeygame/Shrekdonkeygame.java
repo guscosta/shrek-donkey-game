64f6c747fb48dc7d41a901049a14b364fff69c34
hecto_000 <hecto_000@Pocho>
2015-01-31 20:47:09 -0700

    Testing of new classes

--- shrekdonkeygame/src/shrekdonkeygame/Shrekdonkeygame.java
+++ shrekdonkeygame/src/shrekdonkeygame/Shrekdonkeygame.java
@@ -1,5 +1,9 @@
 package shrekdonkeygame;
 
+import BYUI.CIT260.ShrekDonkeyGame.model.Actor;
+import BYUI.CIT260.ShrekDonkeyGame.model.Donkey;
+import BYUI.CIT260.ShrekDonkeyGame.model.Game;
+import BYUI.CIT260.ShrekDonkeyGame.model.Phrase;
 import BYUI.CIT260.ShrekDonkeyGame.model.Player;
 
 public class Shrekdonkeygame {
@@ -11,6 +15,35 @@ public class Shrekdonkeygame {
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
         
+        Game gameOne = new Game();
+        
+        gameOne.setTime(15.00);
+        gameOne.setWinner("Simba");
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
+        
+        Actor actorOne = new Actor();
+        
+        actorOne.setName("Donkey");
+        
+        String actorInfo = actorOne.toString();
+        System.out.println(actorInfo);
+        
+        Phrase phraseOne = new Phrase();
+        
+        phraseOne.setPhraseType("Friendly");
+        phraseOne.setMessage("You are a good friend");
+        
+        String phraseInfo = phraseOne.toString();
+        System.out.println(phraseInfo);
+        
+        Donkey donkeyKick = new Donkey();
+        
+        donkeyKick.setKick(1000);
+        
+        String donkeyInfo = donkeyKick.toString();
+        System.out.println(donkeyInfo);        
     }
     
 }
